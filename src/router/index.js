8c07d62556d945235536c0dec380927e183d63e5
José Luis González <joseluisgonsan@gmail.com>
2022-12-25 10:36:13 +0100

    Cambios router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import AuthView from '../views/AuthView.vue'
+import AuthView from '@/views/AuthView.vue'
+import BoardView from '@/views/BoardView.vue'
+import CardView from '@/views/CardView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,20 +16,23 @@ const router = createRouter({
     {
       path: '/board',
       name: 'Board',
-      component: () => import(/* webpackChunkName: "Board" */ '../views/BoardView.vue'),
+      //component: () => import(/* webpackChunkName: "Board" */ '../views/BoardView.vue'),
+      component: BoardView,
       // Mi ruta hija
       children: [
         {
           path: 'card/:id',
           name: 'Card',
-          component: () => import(/* webpackChunkName: "Card" */ '../views/CardView.vue'),
+          //component: () => import(/* webpackChunkName: "Card" */ '../views/CardView.vue'),
+          component: CardView,
         },
       ],
     },
     {
       path: '/:pathMatch(.*)*', // Cualquier ruta que no sea las anteriores
       name: 'NotFound',
-      component: () => import(/* webpackChunkName: "NotFound" */ '../views/NotFoundView.vue'),
+      //component: () => import(/* webpackChunkName: "NotFound" */ '../views/NotFoundView.vue'),
+      component: NotFoundView,
     },
   ],
 })
