0b340daeef95b5f21c1185c14c65b2763eeebb58
José Luis González <joseluisgonsan@gmail.com>
2022-12-28 12:32:14 +0100

    Destacar tarjetas completadas y caducadas

--- src/components/BoardCard.vue
+++ src/components/BoardCard.vue
@@ -2,6 +2,7 @@
   <div
     class="mb-3 cursor-move rounded border border-white bg-white px-3 pt-3 pb-5 shadow"
     @click="openCard"
+    :class="{ done: card.done, overdue }"
   >
     <h4 class="text-md font-sans font-semibold tracking-wide text-gray-700">
       {{ card.name }}
@@ -37,9 +38,21 @@
 
   const cardDate = computed(() => new Date(+props.card.date).toLocaleDateString('es-ES'))
 
+  const overdue = computed(() => {
+    if (props.card.done) return false
+    return props.card.date < Date.now()
+  })
+
   function openCard() {
     router.push({ name: 'Card', params: { id: props.card.id } })
   }
 </script>
 
-<style scoped></style>
+<style scoped>
+  .done {
+    @apply bg-green-200;
+  }
+  .overdue {
+    @apply bg-red-200;
+  }
+</style>
